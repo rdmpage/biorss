4c56dc4b89c26aafd77d450366cb15d4ff6d7d96
Roderic Page <rdmpage@gmail.com>
2021-11-08 12:25:37 +0000

    Environmental variables

---
+++ config.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+// $Id: //
+
+/**
+ * @file config.php
+ *
+ * Global configuration variables (may be added to by other modules).
+ *
+ */
+
+global $config;
+
+// Date timezone
+date_default_timezone_set('UTC');
+
+	
+// Environment----------------------------------------------------------------------------
+// In development this is a PHP file that is in .gitignore, when deployed these parameters
+// will be set on the server
+if (file_exists(dirname(__FILE__) . '/env.php'))
+{
+	include 'env.php';
+}
+
+$config['platform'] = 'local';
+//$config['platform'] = 'cloud';
+
+
+// CouchDB--------------------------------------------------------------------------------
+		
+if ($config['platform'] == 'local')
+{
+		// local
+		$config['couchdb_options'] = array(
+				'database' 	=> 'n8n',
+				'host' 		=> getenv('COUCHDB_HOST'),
+				'port' 		=> getenv('COUCHDB_PORT'),
+				'prefix' 	=> getenv('COUCHDB_PROTOCOL')
+				);		
+}
+
+
+$config['stale'] = false;
+
+// Twitter--------------------------------------------------------------------------------
+$config['twitter_api_key'] = getenv('TWITTER_BEARER_TOKEN');
+
+	
+?>
---
+++ env-template.php
@@ -0,0 +1,18 @@
+<?php
+
+// For local testing put username and password here, rename as
+// env.php and add to .gitignore
+// For production (e.g., Heroku) add these as environment variables.
+
+putenv('COUCHDB_PROTOCOL=http');
+putenv('COUCHDB_HOST=');
+putenv('COUCHDB_PORT=');
+putenv('COUCHDB_USERNAME=');
+putenv('COUCHDB_PASSWORD=');
+
+putenv('NCBI_API_KEY=');
+
+putenv('TWITTER_BEARER_TOKEN=');
+
+
+?>
