473a6223d1e72790ad93157152a84fecc7e75853
Roderic Page <rdmpage@gmail.com>
2022-01-13 17:44:02 +0000

    Filter out older versions

--- harvest-datacite.php
+++ harvest-datacite.php
@@ -1882,123 +1882,141 @@ foreach ($issns as $issn => $journal)
 }';
 	}
 	
-	echo $json;
+	// echo $json;
 
 	$obj = json_decode($json);
 	
 	foreach ($obj->data as $item)
 	{
-	
-		$dataFeedElement = new stdclass;
-		$dataFeedElement->id = 'https://doi.org/' . $item->attributes->doi;
-		$dataFeedElement->url = $dataFeedElement->id;
+		$go = true;
 		
-		$dataFeedElement->datePublished = date(DATE_ISO8601, strtotime($item->attributes->created));		
-		
-		$dataFeedElement->item = new stdclass;			
-		$dataFeedElement->item->doi = $item->attributes->doi;
-		
-		if (isset($item->attributes->identifiers))
+		// check whether this is an older version and, if so, ignore it
+		if (isset($item->attributes->relatedIdentifiers))
 		{
-			foreach ($item->attributes->identifiers as $identifier)
+			foreach ($item->attributes->relatedIdentifiers as $relatedIdentifier)
 			{			
-				if ($identifier->identifierType == "LSID")
+				if ($relatedIdentifier->relationType == "IsVersionOf")
 				{
-					add_to_item($dataFeedElement->item, 'lsid', $identifier->identifier);	
+					echo $item->attributes->doi . " is an older version\n";
+					$go = false;
 				}
 			}
-		}
+		}		
+		
+		if ($go)
+		{
+		
+			$dataFeedElement = new stdclass;
+			$dataFeedElement->id = 'https://doi.org/' . $item->attributes->doi;
+			$dataFeedElement->url = $dataFeedElement->id;
+		
+			$dataFeedElement->datePublished = date(DATE_ISO8601, strtotime($item->attributes->created));		
+		
+			$dataFeedElement->item = new stdclass;			
+			$dataFeedElement->item->doi = $item->attributes->doi;
+		
+			if (isset($item->attributes->identifiers))
+			{
+				foreach ($item->attributes->identifiers as $identifier)
+				{			
+					if ($identifier->identifierType == "LSID")
+					{
+						add_to_item($dataFeedElement->item, 'lsid', $identifier->identifier);	
+					}
+				}
+			}
 								
-		if (isset($item->attributes->titles))
-		{
-			$dataFeedElement->name = full_clean_text($item->attributes->titles[0]->title);
-			$dataFeedElement->item->name = $dataFeedElement->name;
-		}
-
-		if (isset($item->attributes->descriptions))
-		{
-			$dataFeedElement->description = full_clean_text($item->attributes->descriptions[0]->description);
-		}
-		
-		if (isset($item->attributes->creators))
-		{
-			foreach ($item->attributes->creators as $creator)
+			if (isset($item->attributes->titles))
 			{
-				if (isset($creator->name))
+				$dataFeedElement->name = full_clean_text($item->attributes->titles[0]->title);
+				$dataFeedElement->item->name = $dataFeedElement->name;
+			}
+
+			if (isset($item->attributes->descriptions))
+			{
+				$dataFeedElement->description = full_clean_text($item->attributes->descriptions[0]->description);
+			}
+		
+			if (isset($item->attributes->creators))
+			{
+				foreach ($item->attributes->creators as $creator)
 				{
-					add_to_item($dataFeedElement->item, 'author', $creator->name);	
+					if (isset($creator->name))
+					{
+						add_to_item($dataFeedElement->item, 'author', $creator->name);	
+					}
 				}
 			}
-		}
 		
-		if (isset($item->attributes->url))
-		{
-			$dataFeedElement->url = $item->attributes->url;	
-			if (preg_match('/\.pdf$/', $item->attributes->url))
+			if (isset($item->attributes->url))
 			{
-				$dataFeedElement->item->pdf = $item->attributes->url;
-			}	
-		}
+				$dataFeedElement->url = $item->attributes->url;	
+				if (preg_match('/\.pdf$/', $item->attributes->url))
+				{
+					$dataFeedElement->item->pdf = $item->attributes->url;
+				}	
+			}
 		
-		if (isset($item->attributes->publicationYear))
-		{
-			add_to_item($dataFeedElement->item, 'publicationYear', $item->attributes->publicationYear);		
-		}
+			if (isset($item->attributes->publicationYear))
+			{
+				add_to_item($dataFeedElement->item, 'publicationYear', $item->attributes->publicationYear);		
+			}
 				
-		if (isset($item->attributes->container))
-		{
+			if (isset($item->attributes->container))
+			{
 		
-			if (isset($item->attributes->container->title))
-			{
-				add_to_item($dataFeedElement->item, 'container-title', $item->attributes->container->title);		
-			}
-					
-			if (isset($item->attributes->container->identifier))
-			{
-				if ($item->attributes->container->identifierType == "ISSN")
+				if (isset($item->attributes->container->title))
 				{
-					add_to_item($dataFeedElement->item, 'issn', $item->attributes->container->identifier);		
+					add_to_item($dataFeedElement->item, 'container-title', $item->attributes->container->title);		
 				}
-			}
+					
+				if (isset($item->attributes->container->identifier))
+				{
+					if ($item->attributes->container->identifierType == "ISSN")
+					{
+						add_to_item($dataFeedElement->item, 'issn', $item->attributes->container->identifier);		
+					}
+				}
 			
-			if (isset($item->attributes->container->volume))
-			{
-				add_to_item($dataFeedElement->item, 'volume', $item->attributes->container->volume);		
-			}
+				if (isset($item->attributes->container->volume))
+				{
+					add_to_item($dataFeedElement->item, 'volume', $item->attributes->container->volume);		
+				}
 
-			/*
-			if (isset($item->issue))
-			{
-				add_to_item($dataFeedElement->item, 'issue', $item->issue);		
-			}
-			*/
+				/*
+				if (isset($item->issue))
+				{
+					add_to_item($dataFeedElement->item, 'issue', $item->issue);		
+				}
+				*/
 
-			if (isset($item->attributes->container->firstPage))
-			{
-				add_to_item($dataFeedElement->item, 'firstPage', $item->attributes->container->firstPage);		
-			}
+				if (isset($item->attributes->container->firstPage))
+				{
+					add_to_item($dataFeedElement->item, 'firstPage', $item->attributes->container->firstPage);		
+				}
 
-			if (isset($item->attributes->container->lastPage))
-			{
-				add_to_item($dataFeedElement->item, 'lastPage', $item->attributes->container->lastPage);		
-			}
+				if (isset($item->attributes->container->lastPage))
+				{
+					add_to_item($dataFeedElement->item, 'lastPage', $item->attributes->container->lastPage);		
+				}
 			
 			
-			// Zenodo/DataCite often don't know about journal
+				// Zenodo/DataCite often don't know about journal
 			
-			if (!isset($dataFeedElement->item->container) && isset($dataFeedElement->item->issn))
-			{
-				$dataFeedElement->item->container = $issns[$dataFeedElement->item->issn];
-			}
+				if (!isset($dataFeedElement->item->container) && isset($dataFeedElement->item->issn))
+				{
+					$dataFeedElement->item->container = $issns[$dataFeedElement->item->issn];
+				}
 						
-		}
+			}
 
 		
-		$dataFeed->dataFeedElement[] = $dataFeedElement;
+			$dataFeed->dataFeedElement[] = $dataFeedElement;
+		}
 	
 	}
 	
-	print_r($dataFeed);
+	//print_r($dataFeed);
 	
 	$json_filename = $latest_dir . '/' . $issn . '.json';
  
